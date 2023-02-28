99772c16d54d85ca9cec52d8212d64b0fd360ad0
Josund04 <89245272+Josund04@users.noreply.github.com>
2023-02-28 10:37:51 +0100

    Add files via upload

---
+++ atm/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Vällkommen, vill du registrera ett nytt kort eller göra ett uttag? Tryck 1 för nytt kort, och 2 för att göra ett uttag.");
+Console.ReadLine();
