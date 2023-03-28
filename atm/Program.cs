1828ef0365ad62bafb97c6239ec0a4610d048aea
leonkarlsson1 <>
2023-03-28 11:26:05 +0200

    lite

--- atm/Program.cs
+++ atm/Program.cs
@@ -1,3 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Vällkommen, vill du registrera ett nytt kort eller göra ett uttag? Tryck 1 för nytt kort, och 2 för att göra ett uttag.");
-Console.ReadLine();
+class läsa
+{
+    public static void Main()
+    {
+        int val1;
+
+        Console.WriteLine("Vällkommen, vill du registrera ett nytt kort eller göra ett uttag? Tryck 1 för att registrera ett nytt kort, och 2 för att göra ett uttag.");
+        val1 = Convert.ToString32(Console.ReadLine());
+
+        if (Val1 >= 1)
+        {
+            Console.WriteLine("Ett nytt kort kommer registreras");
+        }
+        else {
+            Console.WriteLine("Hur mycket vill du ta ut?");
+        }
+    
+    }
+}
+
